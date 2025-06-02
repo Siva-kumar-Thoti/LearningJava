b647dcbb8455afe8705888efdd6a235cfb152bc0
SivakumarT <sivakumar.t@walmart.com>
2025-06-02 09:30:50 +0530

    JAVA GENERICS- Class 1

--- src/main/java/com/example/LearningJava/JavaGenerics/Generics.java
+++ src/main/java/com/example/LearningJava/JavaGenerics/Generics.java
@@ -1,8 +1,26 @@
 package com.example.LearningJava.JavaGenerics;
 
-public class Generics {
-  public static void main(String[] args) {
+public class Generics<T> {
+  T a=(T) new Object();
+  T[] arr= (T[])new Object[3];
+  //int[] x=(int[])new Object[4]; //Object array cant be Typecasted to primitive datatype array
+  int xyz=(int)new Object();
+  int [] y=new int[10];
 
+  public static void main(String[] args) {
+    Object obj="siva";
+    obj=10;
+    int x=10;
+    //String str1=(String) x; // can't TypeCast
+    //String str2=(String) obj; // can't TypeCast
+
+//    System.out.println(str1);
+
+
+
+    Generics<String> gen = new Generics<>();
+    gen.arr= new String[3];
+    String abc=gen.arr[0];
   }
 
 }
